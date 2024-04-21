3434ffc00e81e1f1e9b4667178c04c78b63db40b
Dasxunya <79106085+Dasxunya@users.noreply.github.com>
2024-04-21 15:43:28 +0300

    сделан рефакторинг UnitTestsGenerator

--- src/main/java/org/dasxunya/diploma/Generator/UnitTestsGenerator.java
+++ src/main/java/org/dasxunya/diploma/Generator/UnitTestsGenerator.java
@@ -5,48 +5,18 @@ import com.intellij.psi.*;
 import java.util.StringJoiner;
 
 public class UnitTestsGenerator {
+
     private final StringBuilder stringBuilder;
 
     public UnitTestsGenerator() {
         this.stringBuilder = new StringBuilder();
     }
 
-    private String capitalize(String str) {
+    private String Capitalize(String str) {
         if (str == null || str.isEmpty()) {
             return str;
         }
         return Character.toUpperCase(str.charAt(0)) + str.substring(1);
-    }
-
-    private String generateUnitTest(PsiMethod method) {
-        if (method == null) {
-            throw new IllegalArgumentException("PsiMethod is null");
-        }
-
-        String methodName = method.getName();
-        PsiType returnType = method.getReturnType();
-        PsiParameter[] parameters = method.getParameterList().getParameters();
-
-        String testMethodName = "test" + capitalize(methodName);
-        StringJoiner parameterList = new StringJoiner(", ");
-
-        for (PsiParameter parameter : parameters) {
-            String type = parameter.getType().getPresentableText();
-            String name = parameter.getName();
-            // Здесь мы используем просто имена переменных,
-            // в реальном случае может потребоваться инициализация с дефолтными значениями
-            parameterList.add(type + " " + name);
-        }
-
-        StringBuilder testMethodBuilder = new StringBuilder();
-        testMethodBuilder.append("@org.junit.jupiter.api.Test\n");
-        testMethodBuilder.append("public void ").append(testMethodName).append("() {\n");
-        testMethodBuilder.append("    // TODO: создать экземпляр тестируемого класса и инициализировать параметры\n");
-        testMethodBuilder.append("    // TODO: вызвать тестируемый метод с параметрами: ").append(parameterList).append("\n");
-        testMethodBuilder.append("    // TODO: добавить проверки утверждений с помощью assert\n");
-        testMethodBuilder.append("}\n");
-
-        return testMethodBuilder.toString();
     }
 
     public String Generate(PsiElement element) {
@@ -64,9 +34,33 @@ public class UnitTestsGenerator {
 
     public String Generate(PsiMethod psiMethod) {
         stringBuilder.setLength(0);
+        if (psiMethod == null)
+            throw new IllegalArgumentException("Передана пустая ссылка на PsiMethod");
         stringBuilder.append("Метод: ").append(psiMethod.getName());
         stringBuilder.append("Сигнатура: ").append(psiMethod.getSignature(PsiSubstitutor.EMPTY));
-        stringBuilder.append("Юнит тест: ").append(generateUnitTest(psiMethod));
+        stringBuilder.append("Юнит тест: \n");
+
+        //region Генерация тела юнит теста
+        String methodName = psiMethod.getName();
+        PsiType returnType = psiMethod.getReturnType();
+        PsiParameter[] parameters = psiMethod.getParameterList().getParameters();
+        String testMethodName = "test" + Capitalize(methodName);
+        StringJoiner parameterList = new StringJoiner(", ");
+        for (PsiParameter parameter : parameters) {
+            String type = parameter.getType().getPresentableText();
+            String name = parameter.getName();
+            // Здесь мы используем просто имена переменных,
+            // в реальном случае может потребоваться инициализация с дефолтными значениями
+            parameterList.add(type + " " + name);
+        }
+        stringBuilder.append("@org.junit.jupiter.api.Test\n");
+        stringBuilder.append("public void ").append(testMethodName).append("() {\n");
+        stringBuilder.append("    // TODO: создать экземпляр тестируемого класса и инициализировать параметры\n");
+        stringBuilder.append("    // TODO: вызвать тестируемый метод с параметрами: ").append(parameterList).append("\n");
+        stringBuilder.append("    // TODO: добавить проверки утверждений с помощью assert\n");
+        stringBuilder.append("}\n");
+        //endregion
+
         return stringBuilder.toString();
     }
 
@@ -77,4 +71,5 @@ public class UnitTestsGenerator {
             stringBuilder.append(this.Generate(method));
         return stringBuilder.toString();
     }
+
 }
